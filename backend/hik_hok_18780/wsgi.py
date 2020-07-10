9f5d2c7b9e85cb6e4e6dbacf509b73b013b0ba28
Crowdbotics <team@crowdbotics.com>
2020-07-10 04:17:18 +0000

    Initial commit.

---
+++ backend/hik_hok_18780/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for hik_hok_18780 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hik_hok_18780.settings')
+
+application = get_wsgi_application()
